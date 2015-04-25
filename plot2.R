930b7ec478f0390e3f7f2d9bfc6faa092f930d94
Vibhas1988 <trivedivibhas@gmail.com>
2015-04-25 15:34:50 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,18 +1,10 @@
-## This first line will likely take a few seconds. Be patient!
-if(!exists("NEI")){
-  NEI <- readRDS("./data/summarySCC_PM25.rds")
-}
-if(!exists("SCC")){
-  SCC <- readRDS("./data/Source_Classification_Code.rds")
-}
+NEI <- readRDS("C:/Users/vibhas/R/exdata_data_NEI_data/summarySCC_PM25.rds")
+SCC <- readRDS(""C:/Users/vibhas/R/exdata_data_NEI_data/Source_Classification_Code.rds")
 
-# Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? 
-# Use the base plotting system to make a plot answering this question.
+NEI_fips  <- NEI[NEI$fips=="24510", ]
 
-subsetNEI  <- NEI[NEI$fips=="24510", ]
-
-aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
+aggregated_fips <- aggregate(NEI$Emissions ~ NEI$year, data=NEI_fips, FUN=sum)
 
 png('plot2.png')
-barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
+barplot(height=aggregated_fips$Emissions, names.arg=aggregated_fips$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
 dev.off()
